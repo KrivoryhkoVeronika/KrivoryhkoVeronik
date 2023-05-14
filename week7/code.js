e380075734b304a17827e02c9d69cd211a0f4b16
KrivoryhkoVeronika <133563583+KrivoryhkoVeronika@users.noreply.github.com>
2023-05-14 11:08:46 +0300

    Update code.js

--- week7/code.js
+++ week7/code.js
@@ -9,13 +9,12 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 3000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(0xCBEFFFD);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const pyramidgeometry=new THREE.CylinderGeometry(0, 0.8, 2, 4); 
-const pyramidmaterial=new THREE.MeshLambertMaterial( {color: 0xF555844});
+const pyramidmaterial=new THREE.MeshLambertMaterial( {color: 0xF566687});
 const pyramidmesh=new THREE.Mesh(pyramidgeometry, pyramidmaterial);
 pyramidmesh.position.set(0, 2, -10); 
 scene.add(pyramidmesh); 
