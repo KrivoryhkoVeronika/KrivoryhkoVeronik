598240f494ac6d8d8d2f787152bc1be3ea1a355e
KrivoryhkoVeronika <133563583+KrivoryhkoVeronika@users.noreply.github.com>
2023-05-14 11:00:48 +0300

    Update code.js

--- week7/code.js
+++ week7/code.js
@@ -9,6 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 3000 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xCBEFFF);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
